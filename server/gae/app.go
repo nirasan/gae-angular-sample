94aee60f3021e2a639767e1d9ebc4b1ff65797b0
nirasan <niranirao@gmail.com>
2018-02-23 19:06:58 +0900

    impl hell handler

--- server/gae/app.go
+++ server/gae/app.go
@@ -1,12 +1,41 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
+	"github.com/labstack/echo"
+	"net/http"
 )
 
 func init() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "hello world")
-    })
+    // 軽量なウェブアプリケーションフレームワーク echo を使う
+    // 素の net/http に比べてパラメータの bind や Json の出力を便利になる
+	e := echo.New()
+
+	// ルート定義
+	e.GET("/hello", helloHandler)
+
+	// 全リクエストを echo で処理する
+	http.Handle("/", e)
+}
+
+// helloHandler のリクエスト
+type helloRequest struct {
+    // クエリストリングの name 要素を受け取る
+	Name string `query:"name"`
+}
+
+// helloHandler のレスポンス
+type helloResponse struct {
+	Message string
+}
+
+// echo のハンドラ型を定義
+func helloHandler(c echo.Context) error {
+    // 入力を受け取って構造体に入れる
+	req := new(helloRequest)
+	c.Bind(req)
+
+	// ステータスコード 200 で JSON を返す
+	return c.JSON(http.StatusOK, helloResponse{
+		Message: "hello " + req.Name,
+	})
 }
